0e87c535189f987cb7bd56ddc02385bd3cb831ba
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2019-11-29 12:58:34 +0700

    Create gojek3.php

---
+++ gojek3.php
@@ -0,0 +1,82 @@
+<?php
+
+error_reporting(0);
+include ("func.php");
+echo "\e SCRIPT GOJEK AUTO REGISTER + AUTO CLAIM VOUCHER\n";
+echo "\n";
+nope:
+echo "\e[?] Masukkan Nomor HP Anda (1) : ";
+$nope = trim(fgets(STDIN));
+$cek = cekno($nope);
+if ($cek == false)
+    {
+    echo "\e[x] Nomor Telah Terdaftar\n";
+			goto nope;
+    }
+  else
+    {
+echo "\e[!] Siapkan OTPmu\n";
+sleep(5);
+$register = register('1'.$nope);
+if ($register == false)
+    {
+    echo "\e[x] Failed Get OTP!\n";
+    }
+  else
+    {
+    otp:
+    echo "\e[!] Masukkan Kode Verifikasi (OTP) : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[x] Kode Verifikasi Salah\n";
+        goto otp;
+        }
+      else
+        {
+	    $claims = food($verif);
+		echo "\e[!] Trying to redeem Voucher : ".$claims." !\n";
+        sleep(3);
+        $claim = claims($verif,$claims);
+        if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+			      goto ride;
+            }else{
+                echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+            ride:
+            $claim = ride($verif);
+            if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+            sleep(3);
+            }else{
+                echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+                sleep(3);
+                goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                
+        }
+    }
+    }
+    }
+
+
+?>
