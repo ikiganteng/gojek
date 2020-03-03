167d660a18c9492cc8162def812cb5a261756c6c
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2020-03-03 02:25:42 -0600

    Update func.php

--- func.php
+++ func.php
@@ -6,12 +6,12 @@ $header[] = "User-Agent: okhttp/3.12.1";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: en-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.34.1"; // ubah sesuai clone lu
+$header[] = "X-AppVersion: 3.48.2"; // ubah sesuai clone lu
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: en_ID";
-$header[] = "X-Location: -6.225623,106.835570";
-$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-Location: -6.224058,106.877913";
+$header[] = "X-Location-Accuracy: 0.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
@@ -64,7 +64,7 @@ function nama()
 function register($no)
     {
     $nama = nama();
-    $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
+    $email = str_replace(" ", "", $nama) . mt_rand(1000, 9999);
     $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$no.'","signed_up_country":"ID"}';
     $register = request("/v5/customers", "", $data);
     if ($register['success'] == 1)
@@ -224,8 +224,23 @@ function claims($token,$voc)
     }
      function pengen($token)
     {
-    $data = '{"promo_code":"PAKEGOJEK"}';    
+    $data = '{"promo_code":"G-7RCBDYN"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
+    if ($claim['success'] == 1)
+        {
+        return $claim['data']['message'];
+        }
+      else
+        {
+      save("error_log.txt", json_encode($claim));
+        return false;
+        }
+    }
+
+function reff($token)
+    {
+    $data = '{"referral_code":"G-75SR565"}';    
+    $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
     if ($claim['success'] == 1)
         {
         return $claim['data']['message'];
@@ -240,7 +255,7 @@ function claims($token,$voc)
 	function cekno($no)
     {
 	$token = '5993944e-50c7-4f93-bb81-2f2acb206c7a';
-    $claim = request("/wallet/qr-code?phone_number=%2B1".$no, $token, null);
+    $claim = request("/wallet/qr-code?phone_number=%2B".$no, $token, null);
     if ($claim['data'] == null)
         {
 		return true;
